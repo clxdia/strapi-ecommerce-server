b91d75d753d11c63607e7d53cdb71b408592b6e7
blue cat <01claudia.parravano@gmail.com>
2023-03-13 13:05:22 +0100

    fix server unhealthy 1

---
+++ config/env/production/database.js
@@ -0,0 +1,29 @@
+const { parse } = require("pg-connection-string");
+
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_HOST"));
+
+  return {
+    connection: {
+      client: "postgres",
+      connection: {
+        host,
+        port,
+        database,
+        user,
+        password,
+      },
+      debug: false,
+      acquireConnectionTimeout: 5000,
+      pool: {
+        min: 0,
+        max: 10,
+        createTimeoutMillis: 8000,
+        acquireTimeoutMillis: 8000,
+        idleTimeoutMillis: 8000,
+        reapIntervalMillis: 1000,
+        createRetryIntervalMillis: 100,
+      },
+    },
+  };
+};
