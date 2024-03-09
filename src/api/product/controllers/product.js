3bf83e494a0cd358ded7eb4d5462a5448fa97a95
clxdia <claudiaparravano2001@gmail.com>
2024-03-09 18:51:46 +0100

    added feature to findOne product by slug instead of id

--- src/api/product/controllers/product.js
+++ src/api/product/controllers/product.js
@@ -6,4 +6,20 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::product.product');
+module.exports = createCoreController('api::product.product', ({ strapi }) => ({
+    async findOne(ctx) {
+        const { id } = ctx.params;
+
+        const entity = await strapi.db.query('api::product.product').findOne(
+            {
+                where: { slug: id }
+                // where: { id: id }
+            }
+        );
+        const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+        return this.transformResponse(sanitizedEntity)
+    }
+}));
+
+// module.exports = createCoreController('api::product.product');
